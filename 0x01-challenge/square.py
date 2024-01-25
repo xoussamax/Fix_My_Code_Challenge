65bbb8843d80116e6a030fd97b767f2314b381e5
oussama amahrouch <oussama_98@hotmail.fr>
2024-01-25 02:09:01 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
+""" Defines Square class."""
 
 class Square():
-    
+    """ Square class."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
+        """ Area of the square """
         return self.width * self.height
 
     def permiter_of_my_square(self):
+        """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
 
